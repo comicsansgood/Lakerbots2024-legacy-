a5c9251931b2cb1a705cd2a98785fea71ed99436
comicsansgood <masonman0810@gmail.com>
2024-01-12 15:37:05 -0500

    1/12 push intake code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,43 +2,52 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase{
     
-    public CANSparkFlex intakeMotor;
-    public SparkPIDController pidController;
+    public CANSparkFlex intakeSpinMotor;
+    public CANSparkFlex intakeWristMotor;
+    public SparkPIDController intakeWristPidController;
     public double closedPosition = 0;
     public double openPosition = 1;
     public double tolerance = 0.1;
+    public final double WristInSetpoint = 0; //TODO tune this
+    public final double WristOutSetpoint = 1;//TODO tune this
     public double setpoint;
     
     public IntakeSubsystem(){
-        intakeMotor = new CANSparkFlex(0, MotorType.kBrushless);
-        pidController = intakeMotor.getPIDController();
-        pidController.setP(0.1);
+        intakeSpinMotor = new CANSparkFlex(4, MotorType.kBrushless);
+        intakeWristMotor = new CANSparkFlex(5, MotorType.kBrushless);
+        intakeWristPidController = intakeWristMotor.getPIDController();
+        intakeWristPidController.setP(0.1);
 
     }
 
-    public void intakeClose(){
-        setpoint = 0;
-        pidController.setReference(0, CANSparkFlex.ControlType.kSmartMotion);
+    public void intakeSpin(double x){
+        intakeSpinMotor.set(x);
     }
     
-    public void intakeOpen(){
+    public void intakeStop(){
+        intakeSpinMotor.set(0);
+    }
+
+    public void intakeOut(){
+        setpoint = 0;
+        intakeWristPidController.setReference(0, CANSparkFlex.ControlType.kSmartMotion);
+    }
+    
+    public void intakeIn(){
         setpoint = 1;
-        pidController.setReference(1, CANSparkFlex.ControlType.kSmartMotion);
+        intakeWristPidController.setReference(1, CANSparkFlex.ControlType.kSmartMotion);
     }
-
    
-    public double intakeGetPosition(){
-        return intakeMotor.getEncoder().getPosition();
+    public double intakeWristGetPosition(){
+        return intakeWristMotor.getEncoder().getPosition();
     }
 
-    public boolean intakeAtTargetPosition(){
-        return(Math.abs(setpoint - intakeMotor.getEncoder().getPosition()) < tolerance);
+    public boolean intakeWristAtTargetPosition(){
+        return(Math.abs(setpoint - intakeWristMotor.getEncoder().getPosition()) < tolerance);
     }
 
 
